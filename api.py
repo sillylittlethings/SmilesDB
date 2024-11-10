f6fec19d826cd580535d5d866d694e126b1286f4
William Choi-Kim <will.choi.kim@gmail.com>
2024-11-10 01:24:26 -0600

    basic api

--- api.py
+++ api.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
 
 app = Flask(__name__)
 
+with open("data/processedSmiles.txt", "r") as f:
+    proteinList = list(map(lambda a : a.strip(), f.readlines()))
+    proteinList.sort(key=lambda x : len(x))
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+@app.route("/api")
+def docs():
+    return render_template("docs.html")
+
+@app.route("/api/full")
+def full():
+    return jsonify(proteinList)
+
+@app.route("/api/<string:endpoint>/<int:num>")
+def api(endpoint, num):
+    match endpoint:
+        case "random":
+            return jsonify(random.sample(proteinList, min(num, len(proteinList))))
+        case "short":
+            return jsonify(proteinList[:num])
+        case "long":
+            return jsonify(proteinList[-num:])
+        case _:
+            return "INVALID ENPOINT", 404
