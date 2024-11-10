9453603086c59d3198b37493c92b101a0da5d026
William Choi-Kim <will.choi.kim@gmail.com>
2024-11-10 09:15:51 -0600

    removed invalid

--- scrapers/process.py
+++ scrapers/process.py
@@ -1,6 +1,6 @@
-with open("smiles.txt", "r") as f:
+with open("data/smiles.txt", "r") as f:
     smiles = f.readlines()
 
 filtered = filter(lambda a : a.strip() != "", smiles)
-with open("processedSmiles.txt", "w") as f:
+with open("data/processedSmiles.txt", "w") as f:
     f.writelines(filtered)
